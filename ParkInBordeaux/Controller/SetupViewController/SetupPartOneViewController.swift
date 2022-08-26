730a22b03850555056c1da00fd8eedb964c4f41e
Clmtg <garciaclement.c@icloud.com>
2022-08-26 09:40:11 +0200

    Init implementation one time setup view after first launch

---
+++ ParkInBordeaux/Controller/SetupViewController/SetupPartOneViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SetupPartOneViewController.swift
+//  ParkInBordeaux
+//
+//  Created by Clément Garcia on 25/08/2022.
+//
+
+import UIKit
+
+class SetupPartOneViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ParkInBordeaux/Controller/SetupViewController/SetupViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SetupViewController.swift
+//  ParkInBordeaux
+//
+//  Created by Clément Garcia on 25/08/2022.
+//
+
+import UIKit
+
+class SetupViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
